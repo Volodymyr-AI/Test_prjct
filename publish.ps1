9b2797fd19deb53edf2f14458d009f28480bdf9e
Volodymyr <alternativeqoob@gmail.com>
2026-03-11 15:35:52 +0200

    Added script for publishing project with singlefile flag

---
+++ publish.ps1
@@ -0,0 +1,5 @@
+dotnet publish .\Scraper.Worker\Scraper.Worker.csproj `
+  -c Release -r win-x64 --self-contained true `
+  -p:PublishSingleFile=true `
+  -p:IncludeNativeLibrariesForSelfExtract=true `
+  -o .\target\SoftoriaScraper
